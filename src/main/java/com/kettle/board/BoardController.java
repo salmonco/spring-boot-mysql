9308f297fde4fe4233dd7bae1a2230e23b2ed09a
salmonco <xkqjsslsek80@naver.com>
2024-01-31 17:58:13 +0900

    print hello

---
+++ src/main/java/com/kettle/board/BoardController.java
@@ -0,0 +1,14 @@
+package com.kettle.board;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class BoardController {
+    @RequestMapping(value = "/bhome")
+    public String bhome(){
+        System.out.println("bhome");
+        return "index.html";
+    }
+
+}
---
+++ src/main/java/com/kettle/demo/DemoApplication.java
@@ -0,0 +1,16 @@
+package com.kettle.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.context.annotation.ComponentScan;
+
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
+@ComponentScan(basePackages = {"com.kettle"})
+public class DemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoApplication.class, args);
+    }
+
+}
